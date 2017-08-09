bfd02d0644812180cd273a89d07c512dfd44c527
Matt Baer <matt@baer.works>
2017-08-08 21:20:36 -0400

    Prevent choking on null `..._at` properties

--- posts.go
+++ posts.go
@@ -13,22 +13,22 @@ const (
 
 // Post represents a post in Ghost
 type Post struct {
-	ID          int       `json:"id"`
-	UUID        string    `json:"uuid"`
-	Title       string    `json:"title"`
-	Slug        string    `json:"slug"`
-	Markdown    string    `json:"markdown"`
-	HTML        string    `json:"html"`
-	Featured    intBool   `json:"featured"`
-	Page        intBool   `json:"page"`
-	Status      string    `json:"status"`
-	Language    string    `json:"language"`
-	PublishedAt time.Time `json:"published_at"`
-	PublishedBy int       `json:"published_by"`
-	CreatedAt   time.Time `json:"created_at"`
-	CreatedBy   int       `json:"created_by"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	UpdatedBy   int       `json:"updated_by"`
+	ID          int        `json:"id"`
+	UUID        string     `json:"uuid"`
+	Title       string     `json:"title"`
+	Slug        string     `json:"slug"`
+	Markdown    string     `json:"markdown"`
+	HTML        string     `json:"html"`
+	Featured    intBool    `json:"featured"`
+	Page        intBool    `json:"page"`
+	Status      string     `json:"status"`
+	Language    string     `json:"language"`
+	PublishedAt *time.Time `json:"published_at"`
+	PublishedBy int        `json:"published_by"`
+	CreatedAt   *time.Time `json:"created_at"`
+	CreatedBy   int        `json:"created_by"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+	UpdatedBy   int        `json:"updated_by"`
 }
 
 type intBool bool
